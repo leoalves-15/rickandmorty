032dfa5629736c8792f5c6e41ce17c97d27149bc
leandroalves <leandro.alves@lighthouseit.com.br>
2022-05-23 14:46:51 -0300

    save

--- src/main.js
+++ src/main.js
@@ -1,34 +1,12 @@
-// import Vue from 'vue'
-// import App from './App.vue'
-// import router from './router'
-// import { createProvider } from './vue-apollo'
-
-// Vue.config.productionTip = false
-
-// new Vue({
-//   router,
-//   apolloProvider: createProvider(),
-//   render: h => h(App)
-// }).$mount('#app')
-
-import { createApp, provide, h } from 'vue'
-import { DefaultApolloClient } from '@vue/apollo-composable'
-import { ApolloClient, InMemoryCache } from '@apollo/client/core'
+import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import { createProvider } from './vue-apollo'
 
-const cache = new InMemoryCache()
+Vue.config.productionTip = false
 
-const apolloClient = new ApolloClient({
-  cache,
-  uri: '<https://rickandmortyapi.com/graphql>',
-})
-
-const app = createApp({
-  setup () {
-    provide(DefaultApolloClient, apolloClient)
-  },
-
-  render: () => h(App),
-})
-
-app.mount('#app');
+new Vue({
+  router,
+  apolloProvider: createProvider(),
+  render: h => h(App)
+}).$mount('#app')
